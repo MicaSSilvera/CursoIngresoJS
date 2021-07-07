9059a637027c26e78418c50b062ca28418394aea
MicaSSilvera <micassilvera@outlook.com>
2021-07-07 15:27:23 -0300

    Terminado el TP1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,30 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precio1 = parseInt(txtIdPrecioUno.value);
+    var precio2 = parseInt(txtIdPrecioDos.value);
+    var precio3 = parseInt(txtIdPrecioTres.value);
+    var resultado;
+    resultado = precio1 + precio2 + precio3;
+    alert("El resultado de la suma es: " + resultado);
 }
 function Promedio () 
 {
-	
+	var precio1 = parseInt(txtIdPrecioUno.value);
+    var precio2 = parseInt(txtIdPrecioDos.value);
+    var precio3 = parseInt(txtIdPrecioTres.value);
+    var resultado;
+    resultado = (precio1 + precio2 + precio3)/3;
+    alert("El resultado del promedio es: " + resultado.toFixed(2));
 }
 function PrecioFinal () 
 {
+	
+	var precio1 = parseInt(txtIdPrecioUno.value);
+    var precio2 = parseInt(txtIdPrecioDos.value);
+    var precio3 = parseInt(txtIdPrecioTres.value);
+    var resultado;
+    resultado = (precio1 + precio2 + precio3)*1.21;
+    alert("El precio final es: " + resultado);
 	
 }
