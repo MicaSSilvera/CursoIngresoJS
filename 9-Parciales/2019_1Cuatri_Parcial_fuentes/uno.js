a5bd485d148774d791f13b13b40e4d7f392560fb
MicaSSilvera <micassilvera@outlook.com>
2021-07-12 10:46:09 -0300

    realizado el parcial 1

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,8 @@
 
 function mostrar()
 {
+	var lado = parseInt(prompt("Ingrese lado"));
+	var perimetro = lado + lado + lado;
+	alert("El valor del perimetro es: " + perimetro);
 
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,7 @@
 
 function mostrar()
 {
-	alert("uno");
+	var lado = parseInt(prompt("Ingrese lado"));
+	var perimetro = lado + lado + lado;
+	alert("El valor del perimetro es: " + perimetro);
 }
