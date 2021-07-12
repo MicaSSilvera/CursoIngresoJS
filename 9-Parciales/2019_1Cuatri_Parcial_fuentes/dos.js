e736cd3afcf656d163fb272022cfe4b045f140b5
MicaSSilvera <micassilvera@outlook.com>
2021-07-12 11:17:55 -0300

    realizo parcial 2

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
-  
+  var pareja1 = prompt("Ingrese su nombre");
+  var pareja2 = prompt(" y el de su pareja");
+  var peso1 = parseInt(prompt("Ingrese su peso"));
+  var peso2 = parseInt(prompt("y el de su pareja"));
+  var suma = peso1 + peso2;
+  var promedio = (peso1 + peso2)/2;
+  alert("ustedes se llaman " + pareja1 + " y " + pareja2 + " pesan " + peso1 + " y " + peso2 + " kilos, que sumados son " + suma + " kilos y el promedio de peso " + promedio);
 }
