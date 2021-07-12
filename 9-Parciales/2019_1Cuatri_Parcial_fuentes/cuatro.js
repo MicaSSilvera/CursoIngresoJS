9594670635e6d3fa9a889434cf9260625fef851a
MicaSSilvera <micassilvera@outlook.com>
2021-07-12 12:46:35 -0300

    realizo parcial 4

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
+    var numero1 = parseInt(prompt("Dame un numero"));
+    var numero2 = parseInt(prompt("otro numero"));
+    
+    if (numero1 == numero2) {
+        alert("Los numeros son iguales: " + numero1 + " y " + numero2);
+    } else if (numero1 > numero2) {
+        var resultadoresta = numero1 - numero2;
+
+        if (resultadoresta > 10) {
+            alert("El resultado de la resta es: " + resultadoresta + " y supero a 10 ");
+        }else{
+            alert("El resultado de la resta es: " + resultadoresta);
+        }
+       
+    } else if (numero1 < numero2) {
+        var resultadosuma = numero1 + numero2;
+        alert("El resultado de la suma es: " + resultadosuma);
+    } 
 
 }
