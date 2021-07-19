f617ad7f690325f722088ece0060a6c339b2b731
MicaSSilvera <micassilvera@outlook.com>
2021-07-19 16:04:10 -0300

    realizo switch2

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -6,9 +6,10 @@ function mostrar()
 		case "Febrero":
 			alert("Este mes no tiene más de 29 días.");
 			break;
-			if (!Febrero){
-				alert("Este mes tiene 30 o más días.");
-			}
+		default:
+		  alert("Este mes tiene 30 o más días");
+
+		
 
 	}
 
