ad8224c33ff394a291d6fa42cf82ef76ab665ff3
MicaSSilvera <micassilvera@outlook.com>
2021-07-20 17:08:30 -0300

    realizo TP6

--- 8-TPs/06-jsAdivinaElNumero2.js
+++ 8-TPs/06-jsAdivinaElNumero2.js
@@ -9,20 +9,63 @@
 Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
-var numeroSecreto; 
-var contadorIntentos;
+var numerosecreto;
+var contador = 0;
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
-
+function comenzar(){
+   
+  numerosecreto = random(1,100);
+   txtIdNumero.value = "";
+   txtIdIntentos.value = "0";
+   contador = 0;
+   console.log("NUMERO ALEATORIO: " + numerosecreto)  
 }
 
-function verificar()
-{
-	
-	
+function random(min, max) {
+  return Math.floor((Math.random() * (max - min + 1)) + min);
+}
 
+function verificar(){
+  
+  var numeroUsuario = parseInt(txtIdNumero.value);
+  contador++;
+  txtIdIntentos.value = contador;
+  
+  if (numeroUsuario == numerosecreto){
+   
+	switch (contador){
+		case 1:
+			alert("usted es un Psíquico");
+			break;
+		case 2:
+			alert("excelente percepción");
+			break;
+		case 3:
+			alert("Esto es suerte");
+			break;
+		case 4:
+			alert("Excelente técnica");
+			break;
+		case 5:
+			alert("usted está en la media");
+			break;
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+			alert("falta técnica");
+			break;
+		default: 
+			alert("afortunado en el amor!!");
+			break;
+	}
+  
+  }else if (numerosecreto > numeroUsuario){
+    alert("falta...");
+  
+  }else{
+    alert("se paso");
+  }
+  
 }
