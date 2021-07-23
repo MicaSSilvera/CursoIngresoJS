2d4d4c98baabbc8d0b0b2b038c9aff2608eec1ba
MicaSSilvera <micassilvera@outlook.com>
2021-07-23 11:25:04 -0300

    realizo while3

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -3,6 +3,9 @@ al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
 	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	while (claveIngresada !== "utn750"){
+		claveIngresada = prompt("Ingrese numero clave");
+	}
+    
 	
 }//FIN DE LA FUNCIÓN
